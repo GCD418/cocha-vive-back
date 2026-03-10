4ff0a3e6ce246c07d310b43b2149fb66682256de
gabriel <patosquare418@gmail.com>
2026-03-09 22:39:03 -0400

    feat: added CategoryModel

--- src/main/java/cocha/vive/backend/model/Category.java
+++ src/main/java/cocha/vive/backend/model/Category.java
@@ -1,4 +1,49 @@
 package cocha.vive.backend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "identifying_icon", nullable = false, columnDefinition = "TEXT")
+    private String identifyingIcon;
+
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Builder.Default
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive = true;
+
+    @Column(name = "modified_by_user_id")
+    private long modifiedByUserId;
 }
