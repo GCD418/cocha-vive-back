ce42366af63fcf9d08d5e66a512627fc67f31d46
gabriel <patosquare418@gmail.com>
2026-03-09 23:13:49 -0400

    feat: added Event entity

--- src/main/java/cocha/vive/backend/model/Event.java
+++ src/main/java/cocha/vive/backend/model/Event.java
@@ -1,16 +1,76 @@
 package cocha.vive.backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Array;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "events")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(name = "short_description", nullable = false)
+    private String shortDescription;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String description;
-    private LocalDateTime dateTime;
-    private double cost;
+
+    @Column(nullable = false)
+    private Integer cost;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "organized_by_user_id", nullable = false)
+    private User organizedByUser;
+
+    @Array(length = 10)
+    @Column(columnDefinition = "text[]")
+    private List<String> tags;
+
+    @Array(length = 30)
+    @Column(name = "photo_links", columnDefinition = "text[]")
+    private List<String> photoLinks;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "event_status", nullable = false, length = 20)
+    private EventStatus eventStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reviewed_by_admin_id")
+    private User reviewedByAdminId;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Builder.Default
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive = true;
+
+    @Column(name = "modified_by_user_id")
+    private long modifiedByUserId;
 }
