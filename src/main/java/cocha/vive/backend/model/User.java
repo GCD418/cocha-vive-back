8cbd7208a1807d7d394bf4af45b95b9af9366199
gabriel <patosquare418@gmail.com>
2026-03-12 11:05:36 -0400

    fix: changed primite datatypes to object oriented ones (lowerCase to UpperCase)

--- src/main/java/cocha/vive/backend/model/User.java
+++ src/main/java/cocha/vive/backend/model/User.java
@@ -20,7 +20,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(nullable = false)
     private String names;
