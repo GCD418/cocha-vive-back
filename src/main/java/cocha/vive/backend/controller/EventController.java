8659791b4c86d474b3df8c2839b2797873b6d423
christian-ledezma <chris.ledezma.s@gmail.com>
2026-03-11 23:35:23 -0400

    Feat: add get by id event

--- src/main/java/cocha/vive/backend/controller/EventController.java
+++ src/main/java/cocha/vive/backend/controller/EventController.java
@@ -1,8 +1,10 @@
 package cocha.vive.backend.controller;
 
+import cocha.vive.backend.exception.ResourceNotFoundException;
 import cocha.vive.backend.model.Event;
 import cocha.vive.backend.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,6 +24,13 @@ public class EventController {
     @PostMapping("/events")
     public Event createEvent(@RequestBody Event event) {
         return eventRepository.save(event);
+    }
+
+    @GetMapping("/events/{id}")
+    public ResponseEntity<Event> getEventById(@PathVariable Long id) {
+        Event event = eventRepository.findById(id)
+                .orElseThrow(() ->new ResourceNotFoundException("Event not exists with id: "+id));
+        return ResponseEntity.ok(event);
     }
 
 }
