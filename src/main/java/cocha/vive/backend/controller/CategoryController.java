f45184d4e1668d915a3166fd0f32f008e91e56e6
gabriel <patosquare418@gmail.com>
2026-03-12 22:48:48 -0400

    docs: improved swagger Documentation for Category

--- src/main/java/cocha/vive/backend/controller/CategoryController.java
+++ src/main/java/cocha/vive/backend/controller/CategoryController.java
@@ -1,10 +1,13 @@
 package cocha.vive.backend.controller;
 
+import cocha.vive.backend.exception.ResourceNotFoundException;
 import cocha.vive.backend.model.Category;
 import cocha.vive.backend.model.dto.CategoryCreateDTO;
 import cocha.vive.backend.service.CategoryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -19,10 +22,10 @@ import java.util.Map;
 @RestController
 @RequestMapping("api/v1/")
 @RequiredArgsConstructor
-@Tag(name = "Category", description = "Category basic CRUD operations")
 public class CategoryController {
     private final CategoryService categoryService;
 
+    @Tag(name = "Category", description = "Category entire entity operations")
     @Operation(summary = "Get all active categories")
     @ApiResponse(responseCode = "200", description = "List of categories retrieved successfully")
     @GetMapping("/categories")
@@ -30,6 +33,7 @@ public class CategoryController {
         return categoryService.getAll();
     }
 
+    @Tag(name = "Category Create", description = "Category entity without administrative parameters")
     @Operation(summary = "Create a new category")
     @ApiResponse(responseCode = "200", description = "Category created successfully")
     @PostMapping("/categories")
@@ -37,16 +41,19 @@ public class CategoryController {
         return categoryService.create(category);
     }
 
+    @Tag(name = "Category", description = "Category entire entity operations")
     @Operation(summary = "Get a Category by name")
     @ApiResponses({
         @ApiResponse(responseCode = "200", description = "Category found"),
-        @ApiResponse(responseCode = "404", description = "Category not found")
+        @ApiResponse(responseCode = "404", description = "Category not found",
+        content = @Content(schema = @Schema(implementation = ResourceNotFoundException.class)))
     })
     @GetMapping("/categories/name")
     public Category FindByName(@Parameter(description = "Name of the category to retrieve") @RequestParam(name = "name") String name){
         return categoryService.findByName(name);
     }
 
+    @Tag(name = "Category", description = "Category entire entity operations")
     @Operation(summary = "Soft Delete a Category")
     @ApiResponses({
         @ApiResponse(responseCode = "200", description = "Category deleted successfully"),
