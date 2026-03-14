8b7846f0d1c6a7b79d3a1c89ca17b0d976398d29
gabriel <patosquare418@gmail.com>
2026-03-14 19:43:11 -0400

    feat: query for getting upcoming events

--- src/main/java/cocha/vive/backend/repository/EventRepository.java
+++ src/main/java/cocha/vive/backend/repository/EventRepository.java
@@ -2,8 +2,15 @@ package cocha.vive.backend.repository;
 
 import cocha.vive.backend.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+    @Query(value = "SELECT * FROM events WHERE is_active = true AND date_start > NOW() " +
+        "ORDER BY date_start ASC LIMIT 4", nativeQuery = true)
+    List<Event> findActiveUpcoming();
 }
