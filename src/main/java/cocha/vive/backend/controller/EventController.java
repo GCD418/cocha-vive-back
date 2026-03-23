78e44bdfb5293a01d656648198d810328079de48
christian-ledezma <chris.ledezma.s@gmail.com>
2026-03-23 11:41:23 -0400

    feat: add update-event controller

--- src/main/java/cocha/vive/backend/controller/EventController.java
+++ src/main/java/cocha/vive/backend/controller/EventController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.MediaType;
 
 import java.util.List;
 
@@ -53,6 +54,15 @@ public class EventController {
     @GetMapping("/events/category/{categoryId}")
     public List<Event> getEventsByCategory(@PathVariable Long categoryId) {
         return eventService.getEventsByCategoryId(categoryId);
+    }
+
+    @PutMapping(value = "/events/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PreAuthorize("hasAnyRole('USER')")
+    public ResponseEntity<Event> updateEvent(
+            @PathVariable Long id,
+            @RequestPart("event") EventRequest dto,
+            @RequestPart(value = "images", required = false) List<MultipartFile> images) {
+        return ResponseEntity.ok(eventService.update(id, dto, images));
     }
 
 }
