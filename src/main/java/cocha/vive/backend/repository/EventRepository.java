1228a368f4dc5a0b641cbe5876977be55d66ce45
christian-ledezma <chris.ledezma.s@gmail.com>
2026-04-10 09:44:39 -0400

    Feat: add reposotory methods to specify endpoint calls

--- src/main/java/cocha/vive/backend/repository/EventRepository.java
+++ src/main/java/cocha/vive/backend/repository/EventRepository.java
@@ -12,11 +12,39 @@ import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
-    @Query(value = "SELECT * FROM events WHERE is_active = true AND date_start > NOW() AND event_status = 'APPROVED' " +
-        "ORDER BY date_start ASC LIMIT 4", nativeQuery = true)
-    List<Event> findActiveUpcoming();
+     @Query(value = """
+        SELECT * FROM events
+        WHERE is_active = true
+          AND event_status = 'APPROVED'
+          AND date_end > NOW()
+        ORDER BY date_start ASC
+        """, nativeQuery = true)
+    List<Event> findAllPublic();
 
     List<Event> findByIsActiveTrueAndIsFeaturedTrue();
+
+    @Query(value = """
+        SELECT * FROM events
+        WHERE is_active = true
+          AND date_start > NOW()
+          AND event_status = 'APPROVED'
+        ORDER BY date_start ASC LIMIT 4
+        """, nativeQuery = true)
+    List<Event> findActiveUpcoming();
+
+    @Query(value = """
+        SELECT * FROM events
+        WHERE is_active = true
+          AND organized_by_user_id = :userId
+        ORDER BY date_start DESC
+        """, nativeQuery = true)
+    List<Event> findAllByOrganizedByUserId(@Param("userId") Long userId);
+
+    @Query(value = """
+        SELECT * FROM events
+        ORDER BY created_at DESC
+        """, nativeQuery = true)
+    List<Event> findAllForAdmin();
 
     @Modifying
     @Query("UPDATE Event e SET e.eventStatus = :status, " +
