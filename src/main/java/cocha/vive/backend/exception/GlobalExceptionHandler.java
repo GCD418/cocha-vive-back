4cb390a08dc8bfcaf24de5fb9cf1e2df2f9ad537
christian-ledezma <chris.ledezma.s@gmail.com>
2026-04-21 16:27:34 -0400

    Fix: delete redundant import

--- src/main/java/cocha/vive/backend/exception/GlobalExceptionHandler.java
+++ src/main/java/cocha/vive/backend/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package cocha.vive.backend.exception;
 
 import cocha.vive.backend.model.dto.ErrorResponseDTO;
-import cocha.vive.backend.exception.InvalidRoleTransitionException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
