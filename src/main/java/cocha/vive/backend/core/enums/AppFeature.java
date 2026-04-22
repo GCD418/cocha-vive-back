811e8ec398457c107b2f849fdbaf04f4f5ac5e44
gabriel <patosquare418@gmail.com>
2026-04-22 16:58:07 -0400

    feat: adding feature flag for sending notification

--- src/main/java/cocha/vive/backend/core/enums/AppFeature.java
+++ src/main/java/cocha/vive/backend/core/enums/AppFeature.java
@@ -11,7 +11,8 @@ public enum AppFeature {
     MANAGE_PUBLISHER_REQUESTS("manage-publisher-requests"),
     SEND_WELCOME_EMAIL("send-welcome-email"),
     SEND_NEW_EVENT_NOTIFICATION_EMAIL("send-new-event-notification-email"),
-    SEND_NEW_PUBLISHER_REQUEST_NOTIFICATION_EMAIL("send-new-publisher-request-notification-email");
+    SEND_NEW_PUBLISHER_REQUEST_NOTIFICATION_EMAIL("send-new-publisher-request-notification-email"),
+    NOTIFY_TO_USER_OF_PUBLISHER_REQUEST_CHANGES("notify-to-user-of-publisher-request-changes");
 
     private final String unleashKey;
 
