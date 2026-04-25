0af38fd8b14abd9bffd99ca792ce193f74b58daf
christian-ledezma <chris.ledezma.s@gmail.com>
2026-04-25 01:48:21 -0400

    feat: create FacebookPayload DTO for OAuth response

---
+++ src/main/java/cocha/vive/backend/auth/FacebookPayload.java
@@ -0,0 +1,53 @@
+package cocha.vive.backend.auth;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class FacebookPayload {
+    private String id;
+    private String name;
+
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    private String lastName;
+
+    private String email;
+
+    @JsonProperty("picture")
+    private Picture picture;
+
+    public String getPictureUrl() {
+        return picture != null && picture.data != null ? picture.data.url : null;
+    }
+
+
+    public boolean isPerson() {
+        return firstName != null || lastName != null;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Picture {
+        private PictureData data;
+
+        @Data
+        @NoArgsConstructor
+        @AllArgsConstructor
+        public static class PictureData {
+            private String url;
+        }
+    }
+}
