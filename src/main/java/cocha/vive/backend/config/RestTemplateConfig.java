4ecda53e520fa2d90798ef190cff4857091538a9
christian-ledezma <chris.ledezma.s@gmail.com>
2026-05-02 01:07:34 -0400

    fix: replace RestTemplate witch RestClient and remove redundant ObjectMapper Bean

--- src/main/java/cocha/vive/backend/config/RestTemplateConfig.java
+++ src/main/java/cocha/vive/backend/config/RestTemplateConfig.java
@@ -1,19 +1,13 @@
 package cocha.vive.backend.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestTemplateConfig {
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+    public RestClient restClient() {
+        return RestClient.create();
     }
 }
