2fbd7b2b0abbf441440fadac61e870fab0831843
christian-ledezma <chris.ledezma.s@gmail.com>
2026-05-05 23:10:47 -0400

    fix: unleash key fixed to lowercase

--- src/main/java/cocha/vive/backend/core/enums/AppFeature.java
+++ src/main/java/cocha/vive/backend/core/enums/AppFeature.java
@@ -13,7 +13,7 @@ public enum AppFeature {
     SEND_NEW_EVENT_NOTIFICATION_EMAIL("send-new-event-notification-email"),
     SEND_NEW_PUBLISHER_REQUEST_NOTIFICATION_EMAIL("send-new-publisher-request-notification-email"),
     NOTIFY_TO_USER_OF_PUBLISHER_REQUEST_CHANGES("notify-to-user-of-publisher-request-changes"),
-    FACEBOOK_LOGIN("Facebook-login");
+    FACEBOOK_LOGIN("facebook-login");
 
     private final String unleashKey;
 
