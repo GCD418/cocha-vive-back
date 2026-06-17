dc07277eac329635ec3e11aec178d74789024d7e
christian-ledezma <chris.ledezma.s@gmail.com>
2026-06-16 23:31:32 -0400

    refactor: remove VIEW_UPCOMING_EVENTS from AppFeature enum

--- src/main/java/cocha/vive/backend/core/enums/AppFeature.java
+++ src/main/java/cocha/vive/backend/core/enums/AppFeature.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum AppFeature {
-    VIEW_UPCOMING_EVENTS("view-upcoming-events"),
     SEND_NEW_EVENT_NOTIFICATION_EMAIL("send-new-event-notification-email");
     private final String unleashKey;
 
